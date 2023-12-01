03979b93cee9b63120e918111c7b21cde3337830
dokmy <142666819+dokmy@users.noreply.github.com>
2023-12-01 23:34:28 +0800

    added theme and calendar date picker

--- src/app/components/Search/index.tsx
+++ src/app/components/Search/index.tsx
@@ -3,54 +3,75 @@
 import React, { FormEvent, ChangeEvent } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
+import { DatePicker } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
+
+const newTheme = (theme: any) =>
+  createTheme({
+    ...theme,
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#3f51b5",
+      },
+      secondary: {
+        main: "#f50057",
+      },
+    },
+  });
 
 const Search: React.FC = () => {
   return (
-    <div id="Search" className="flex flex-col w-full bg-gray-800">
+    <div id="Search" className="flex flex-col w-full bg-gray-800 space-y-4">
       <h2 className="text-xl font-semibold font-co text-white">
         Search by situation
       </h2>
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <div>
-          <TextField
-            id="filled-multiline-static"
-            label="Situation"
-            multiline
-            rows={4}
-            placeholder="E.g. My client slips and falls in a shopping mall while working..."
-            variant="outlined"
+      <div id="search-bar">
+        <ThemeProvider theme={newTheme}>
+          <Box
+            component="form"
             sx={{
-              "& .MuiOutlinedInput-input": {
-                color: "white", // Font color of the input text
-              },
-              "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-                borderColor: "gray", // Color of the border (outlined variant)
-              },
-              "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline":
-                {
-                  borderColor: "white", // Color of the border on hover (outlined variant)
-                },
-              "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
-                {
-                  borderColor: "white", // Color of the border when focused (outlined variant)
-                },
-              "& .MuiInputLabel-outlined": {
-                color: "grey", // Color of the label text
-              },
-              "& .MuiInputLabel-outlined.Mui-focused": {
-                color: "grey", // Color of the label text when focused
-              },
+              "& .MuiTextField-root": { m: 0, width: "25ch" },
             }}
-          />
-        </div>
-      </Box>
+            noValidate
+            autoComplete="off"
+          >
+            <div>
+              <TextField
+                id="filled-multiline-static"
+                label="Your search query"
+                multiline
+                rows={4}
+                placeholder="E.g. My client slips and falls in a shopping mall while working..."
+                variant="outlined"
+              />
+            </div>
+          </Box>
+        </ThemeProvider>
+      </div>
+
+      <div id="min-date">
+        <ThemeProvider theme={newTheme}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Minimum date"
+              defaultValue={dayjs("1991-07-11")}
+            />
+          </LocalizationProvider>
+        </ThemeProvider>
+      </div>
+
+      <div id="max-date">
+        <ThemeProvider theme={newTheme}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker label="Maximum date" defaultValue={dayjs()} />
+          </LocalizationProvider>
+        </ThemeProvider>
+      </div>
 
       {/* <form
         //   onSubmit={handleMessageSubmit}
