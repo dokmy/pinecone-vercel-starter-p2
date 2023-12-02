322d84ac68124b2bbdc1383ce91d7f998261a785
dokmy <142666819+dokmy@users.noreply.github.com>
2023-12-02 14:01:57 +0800

    parent can receive all the input

--- src/app/components/Search/index.tsx
+++ src/app/components/Search/index.tsx
@@ -9,37 +9,108 @@ import { createTheme } from "@mui/material/styles";
 import { ThemeProvider } from "@mui/material/styles";
 import { DatePicker } from "@mui/x-date-pickers";
 import dayjs from "dayjs";
+import Autocomplete from "@mui/material/Autocomplete";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import { useState } from "react";
 
-const newTheme = (theme: any) =>
-  createTheme({
-    ...theme,
-    palette: {
-      mode: "dark",
-      primary: {
-        main: "#3f51b5",
-      },
-      secondary: {
-        main: "#f50057",
+interface SearchProps {
+  searchQuery: string;
+  setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
+  selectedMinDate: dayjs.Dayjs;
+  setSelectedMinDate: React.Dispatch<React.SetStateAction<dayjs.Dayjs>>;
+  selectedMaxDate: dayjs.Dayjs;
+  setSelectedMaxDate: React.Dispatch<React.SetStateAction<dayjs.Dayjs>>;
+  cofa: string[];
+  setCofa: React.Dispatch<React.SetStateAction<string[]>>;
+  coa: string[];
+  setCoa: React.Dispatch<React.SetStateAction<string[]>>;
+  coficivil: string[];
+  setcoficivil: React.Dispatch<React.SetStateAction<string[]>>;
+  coficriminal: string[];
+  setCoficriminal: React.Dispatch<React.SetStateAction<string[]>>;
+  cofiprobate: string[];
+  setCofiprobate: React.Dispatch<React.SetStateAction<string[]>>;
+  ct: string[];
+  setCt: React.Dispatch<React.SetStateAction<string[]>>;
+  dc: string[];
+  setDc: React.Dispatch<React.SetStateAction<string[]>>;
+  fc: string[];
+  setFc: React.Dispatch<React.SetStateAction<string[]>>;
+  lt: string[];
+  setLt: React.Dispatch<React.SetStateAction<string[]>>;
+  others: string[];
+  setOthers: React.Dispatch<React.SetStateAction<string[]>>;
+  sortOption: string;
+  setSortOption: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const newTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#3f51b5",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+  components: {
+    MuiFormControlLabel: {
+      styleOverrides: {
+        label: {
+          // Set the color for the label text
+          color: "#c2c2c2",
+        },
       },
     },
-  });
+  },
+});
 
-const Search: React.FC = () => {
+const Search: React.FC<SearchProps> = ({
+  searchQuery,
+  setSearchQuery,
+  selectedMinDate,
+  setSelectedMinDate,
+  selectedMaxDate,
+  setSelectedMaxDate,
+  cofa,
+  setCofa,
+  coa,
+  setCoa,
+  coficivil,
+  setcoficivil,
+  coficriminal,
+  setCoficriminal,
+  cofiprobate,
+  setCofiprobate,
+  ct,
+  setCt,
+  dc,
+  setDc,
+  fc,
+  setFc,
+  lt,
+  setLt,
+  others,
+  setOthers,
+  sortOption,
+  setSortOption,
+}) => {
   return (
-    <div id="Search" className="flex flex-col w-full bg-gray-800 space-y-4">
+    <div
+      id="Search"
+      className="flex flex-col w-full bg-gray-800 space-y-4 overflow-y-scroll h-full p-2"
+    >
       <h2 className="text-xl font-semibold font-co text-white">
         Search by situation
       </h2>
       <div id="search-bar">
         <ThemeProvider theme={newTheme}>
-          <Box
-            component="form"
-            sx={{
-              "& .MuiTextField-root": { m: 0, width: "25ch" },
-            }}
-            noValidate
-            autoComplete="off"
-          >
+          <Box component="form" noValidate autoComplete="off">
             <div>
               <TextField
                 id="filled-multiline-static"
@@ -48,6 +119,9 @@ const Search: React.FC = () => {
                 rows={4}
                 placeholder="E.g. My client slips and falls in a shopping mall while working..."
                 variant="outlined"
+                fullWidth={true}
+                value={searchQuery}
+                onChange={(event) => setSearchQuery(event.target.value)}
               />
             </div>
           </Box>
@@ -59,7 +133,8 @@ const Search: React.FC = () => {
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DatePicker
               label="Minimum date"
-              defaultValue={dayjs("1991-07-11")}
+              value={selectedMinDate}
+              onChange={(newValue) => setSelectedMinDate(dayjs(newValue))}
             />
           </LocalizationProvider>
         </ThemeProvider>
@@ -68,27 +143,325 @@ const Search: React.FC = () => {
       <div id="max-date">
         <ThemeProvider theme={newTheme}>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker label="Maximum date" defaultValue={dayjs()} />
+            <DatePicker
+              label="Maximum date"
+              value={selectedMaxDate}
+              onChange={(newValue) => setSelectedMaxDate(dayjs(newValue))}
+            />
           </LocalizationProvider>
         </ThemeProvider>
       </div>
 
-      {/* <form
-        //   onSubmit={handleMessageSubmit}
-        className="mt-5 mb-5 relative bg-gray-700 rounded-lg"
-      >
-        <input
-          type="text"
-          className="input-glow appearance-none border rounded w-full py-2 text-gray-200 leading-tight focus:outline-none focus:shadow-outline pl-3 pr-10 bg-gray-600 border-gray-600 transition-shadow duration-200"
-          // onChange={handleInputChange}
-        />
+      <div id="sort-by">
+        <ThemeProvider theme={newTheme}>
+          <FormControl>
+            <FormLabel id="sort by">Sort the results by:</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="Relevance"
+              name="radio-buttons-group"
+              value={sortOption}
+              onChange={(event) => setSortOption(event.target.value)}
+            >
+              <FormControlLabel
+                value="Relavance"
+                control={<Radio />}
+                label="Relavance"
+              />
+              <FormControlLabel
+                value="Recency"
+                control={<Radio />}
+                label="Recency"
+              />
+            </RadioGroup>
+          </FormControl>
+        </ThemeProvider>
+      </div>
 
-        <span className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none text-gray-400 box-border">
-          Press ⮐ to send
-        </span>
-      </form> */}
+      <div id="count-options" className="space-y-4">
+        <ThemeProvider theme={newTheme}>
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={court_of_final_appeal}
+            getOptionLabel={(option) => option}
+            filterSelectedOptions
+            value={cofa}
+            onChange={(event, newValue) => setCofa(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Court of Final Appeal"
+                placeholder="Choose an option"
+              />
+            )}
+          />
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={court_of_appeal}
+            getOptionLabel={(option) => option}
+            filterSelectedOptions
+            value={coa}
+            onChange={(event, newValue) => setCoa(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Court of Appeal"
+                placeholder="Choose an option"
+              />
+            )}
+          />
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={court_of_first_instance_civil}
+            getOptionLabel={(option) => option}
+            filterSelectedOptions
+            value={coficivil}
+            onChange={(event, newValue) => setcoficivil(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Court of First Instance - Civil Matters"
+                placeholder="Choose an option"
+              />
+            )}
+          />
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={court_of_first_instance_criminal}
+            getOptionLabel={(option) => option}
+            filterSelectedOptions
+            value={coficriminal}
+            onChange={(event, newValue) => setCoficriminal(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Court of First Instance - Criminal & Appeal Cases"
+                placeholder="Choose an option"
+              />
+            )}
+          />
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={court_of_first_instance_probate}
+            getOptionLabel={(option) => option}
+            filterSelectedOptions
+            value={cofiprobate}
+            onChange={(event, newValue) => setCofiprobate(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Court of First Instance - Probate Matters"
+                placeholder="Choose an option"
+              />
+            )}
+          />
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={competition_tribunal}
+            getOptionLabel={(option) => option}
+            filterSelectedOptions
+            value={ct}
+            onChange={(event, newValue) => setCt(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Competition Tribunal"
+                placeholder="Choose an option"
+              />
+            )}
+          />
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={district_court}
+            getOptionLabel={(option) => option}
+            filterSelectedOptions
+            value={dc}
+            onChange={(event, newValue) => setDc(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="District Court"
+                placeholder="Choose an option"
+              />
+            )}
+          />
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={family_court}
+            getOptionLabel={(option) => option}
+            filterSelectedOptions
+            value={fc}
+            onChange={(event, newValue) => setFc(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Family Court"
+                placeholder="Choose an option"
+              />
+            )}
+          />
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={lands_tribunal}
+            getOptionLabel={(option) => option}
+            filterSelectedOptions
+            value={lt}
+            onChange={(event, newValue) => setLt(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Lands Tribunal"
+                placeholder="Choose an option"
+              />
+            )}
+          />
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={court_others}
+            getOptionLabel={(option) => option}
+            filterSelectedOptions
+            value={others}
+            onChange={(event, newValue) => setOthers(newValue)}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Others"
+                placeholder="Choose an option"
+              />
+            )}
+          />
+        </ThemeProvider>
+      </div>
     </div>
   );
 };
+
+const court_of_final_appeal = ["FACV", "FACC", "FAMV", "FAMC", "FAMP"];
+const court_of_appeal = [
+  "CACV",
+  "CACC",
+  "CAAR",
+  "CASJ",
+  "CAQL",
+  "CAAG",
+  "CAMP",
+];
+const court_of_first_instance_civil = [
+  "HCA",
+  "HCAL",
+  "HCAJ",
+  "HCAD",
+  "HCB",
+  "HCCL",
+  "HCCW",
+  "HCSD",
+  "HCBI",
+  "HCCT",
+  "HCMC",
+  "HCMP",
+  "HCCM",
+  "HCPI",
+  "HCBD",
+  "HCBS",
+  "HCSN",
+  "HCCD",
+  "HCZZ",
+  "HCMH",
+  "HCIP",
+];
+const court_of_first_instance_criminal = [
+  "HCCC",
+  "HCMA",
+  "HCLA",
+  "HCIA",
+  "HCSA",
+  "HCME",
+  "HCOA",
+  "HCUA",
+  "HCED",
+  "HCAA",
+  "HCCP",
+];
+const court_of_first_instance_probate = [
+  "HCAP",
+  "HCAG",
+  "HCCA",
+  "HCEA",
+  "HCRC",
+  "HCCI",
+  "HCCV",
+  "HCUA",
+];
+const competition_tribunal = ["CTAR", "CTEA", "CTA", "CTMP"];
+const district_court = [
+  "DCCJ",
+  "DCCC",
+  "DCDT",
+  "DCTC",
+  "DCEC",
+  "DCEO",
+  "DCMA",
+  "DCMP",
+  "DCOA",
+  "DCPI",
+  "DCPA",
+  "DCSA",
+  "DCZZ",
+  "DCSN",
+];
+const family_court = ["FCMC", "FCJA", "FCMP", "FCAD", "FCRE"];
+const lands_tribunal = [
+  "LDPA",
+  "LDPB",
+  "LDPD",
+  "LDPE",
+  "LDRT",
+  "LDNT",
+  "LDLA",
+  "LDRA",
+  "LDBG",
+  "LDGA",
+  "LDLR",
+  "LDHA",
+  "LDBM",
+  "LDDB",
+  "LDDA",
+  "LDMT",
+  "LDCS",
+  "LDRW",
+  "LDMR",
+  "LDMP",
+];
+const court_others = [
+  "CCDI",
+  "ESCC",
+  "ESS",
+  "FLCC",
+  "FLS",
+  "KCCC",
+  "KCS",
+  "KTCC",
+  "KTS",
+  "LBTC",
+  "OATD",
+  "STCC",
+  "STMP",
+  "STS",
+  "SCTC",
+  "TMCC",
+  "TMS",
+  "WKCC",
+  "WKS",
+];
 
 export default Search;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,41 @@ import { useChat } from "ai/react";
 import InstructionModal from "./components/InstructionModal";
 import { AiFillGithub, AiOutlineInfoCircle } from "react-icons/ai";
 import Search from "./components/Search";
+import dayjs from "dayjs";
 
 const Page: React.FC = () => {
   const [isModalOpen, setModalOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedMinDate, setSelectedMinDate] = useState(dayjs("1991-07-11"));
+  const [selectedMaxDate, setSelectedMaxDate] = useState(dayjs());
+  const [cofa, setCofa] = useState<string[]>([]);
+  const [coa, setCoa] = useState<string[]>([]);
+  const [coficivil, setcoficivil] = useState<string[]>([]);
+  const [coficriminal, setCoficriminal] = useState<string[]>([]);
+  const [cofiprobate, setCofiprobate] = useState<string[]>([]);
+  const [ct, setCt] = useState<string[]>([]);
+  const [dc, setDc] = useState<string[]>([]);
+  const [fc, setFc] = useState<string[]>([]);
+  const [lt, setLt] = useState<string[]>([]);
+  const [others, setOthers] = useState<string[]>([]);
+  const [sortOption, setSortOption] = useState("Relevance");
+
+  console.log({
+    searchQuery,
+    selectedMinDate,
+    selectedMaxDate,
+    cofa,
+    coa,
+    coficivil,
+    coficriminal,
+    cofiprobate,
+    ct,
+    dc,
+    fc,
+    lt,
+    others,
+    sortOption,
+  });
 
   return (
     <div className="flex flex-col justify-between h-screen bg-gray-800 p-2 mx-auto max-w-full">
@@ -29,8 +61,37 @@ const Page: React.FC = () => {
         onClose={() => setModalOpen(false)}
       />
       <div className="flex w-full flex-grow overflow-hidden relative">
-        <div className="w-1/5 p-2">
-          <Search />
+        <div className="w-1/3 p-2">
+          <Search
+            searchQuery={searchQuery}
+            setSearchQuery={setSearchQuery}
+            selectedMinDate={selectedMinDate}
+            setSelectedMinDate={setSelectedMinDate}
+            selectedMaxDate={selectedMaxDate}
+            setSelectedMaxDate={setSelectedMaxDate}
+            cofa={cofa}
+            setCofa={setCofa}
+            coa={coa}
+            setCoa={setCoa}
+            coficivil={coficivil}
+            setcoficivil={setcoficivil}
+            coficriminal={coficriminal}
+            setCoficriminal={setCoficriminal}
+            cofiprobate={cofiprobate}
+            setCofiprobate={setCofiprobate}
+            ct={ct}
+            setCt={setCt}
+            dc={dc}
+            setDc={setDc}
+            fc={fc}
+            setFc={setFc}
+            lt={lt}
+            setLt={setLt}
+            others={others}
+            setOthers={setOthers}
+            sortOption={sortOption}
+            setSortOption={setSortOption}
+          />
         </div>
         <div className="w-2/5 p-2">
           <Chat raw_case_num="2023_HKCFI_2489" />
