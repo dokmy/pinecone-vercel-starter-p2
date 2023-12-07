1f3e2d6b4f3f60d56631aee3a77cfd17cf1542f0
dokmy <142666819+dokmy@users.noreply.github.com>
2023-12-07 14:28:13 +0800

    remove only reading user messages

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -44,7 +44,8 @@ export async function POST(req: Request) {
     const response = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       stream: true,
-      messages: [...prompt, ...messages.filter((message: Message) => message.role === 'user')]
+      // messages: [...prompt, ...messages.filter((message: Message) => message.role === 'user')]
+      messages: [...prompt, ...messages]
     })
     // Convert the response into a friendly text-stream
 
