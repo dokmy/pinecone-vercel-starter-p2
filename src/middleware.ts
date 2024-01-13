5fcf5f13846c090eb54751e0aa7519bfecf3a564
dokmy <142666819+dokmy@users.noreply.github.com>
2024-01-13 14:25:43 +0800

    remove dashboard from publicRoutes. Added env in vercel.

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   apiRoutes: ['/api/chat', '/api/search'],
-  publicRoutes: ["/","/dashboard", "/api/webhook"]
+  publicRoutes: ["/", "/api/webhook"]
 });
  
 export const config = {
